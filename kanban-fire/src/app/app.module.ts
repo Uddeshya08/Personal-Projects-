7478203276150b319e30516761033aafacc27236
Uddeshya08 <uddeshya.us@gmail.com>
2021-07-25 00:16:55 +0530

    Drag drop added

--- kanban-fire/src/app/app.module.ts
+++ kanban-fire/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {MatCardModule} from '@angular/material/card'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TaskComponent } from './task/task.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { TaskComponent } from './task/task.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
